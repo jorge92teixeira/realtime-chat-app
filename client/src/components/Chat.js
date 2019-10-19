9460fd1686fbf1c34d0e2407fa7f39a630beaff2
Jorge Teixeira <jorge92@mailbox.org>
2019-10-19 17:21:51 +0100

    addUser, getUser, removeUser, getUsersInRoom functions

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+  const [userName, setName] = useState('');
+  const [roomName, setRoom] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  const ENDPOINT = 'localhost:5000';
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit('join', { name, room }, () => {
+
+    });
+
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages({ ...messages, message });
+    });
+  }, [messages]);
+
   return (
     <div>
       Chat
