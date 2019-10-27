8623bed03b5ecfcaa338659fe09ba33687e53dcb
Jorge Teixeira <jorge92@mailbox.org>
2019-10-27 22:10:35 +0000

    Messages component scss

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ScrollToBottom from 'react-scroll-to-bottom';
 import Message from '../Message/Message';
 
-// import './Messages.css';
-
 const Messages = ({ messages, name }) => (
   <ScrollToBottom className="messages">
     {
